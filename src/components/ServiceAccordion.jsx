38ef81278cf03861c0d5ba906daf129bb505f926
almiraazs <aalmirafiraa@gmail.com>
2025-02-01 20:54:01 +0700

    service

--- src/components/ServiceAccordion.jsx
+++ src/components/ServiceAccordion.jsx
@@ -5,17 +5,21 @@ import DanaSosial from "./Service/DanaSosial";
 import DanaPendidikan from "./Service/DanaPendidikan";
 
 const ServiceAccordion = () => {
-    return (
-        <div className="max-w-3x1 mx-auto my-10">
-            <h2 className="text-2x1 font-bold text-center mb-6">Layanan Kami</h2>
-            <div className="space-y-4">
-                <Simpanan />
-                <Pinjaman />
-                <DanaSosial />
-                <DanaPendidikan />
-            </div>
-        </div>
-    );
+  return (
+    <div className="max-w-3x1 mx-auto my-10">
+      <h1 className="text-3xl font-extrabold text-center text-gray-800 mb-12">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-gray-600 to-gray-600">
+          Our Services
+        </span>
+      </h1>
+      <div className="space-y-4">
+        <Simpanan />
+        <Pinjaman />
+        <DanaSosial />
+        <DanaPendidikan />
+      </div>
+    </div>
+  );
 };
 
 export default ServiceAccordion;
